8a4a15e287be07b932ebaf2af2564cfe90d5cdc2
Jawaad Ahmad <jawaad.ahmad@gmail.com>
2017-12-28 22:55:45 -0600

    Issue #1: Added minimal sed processing on wpa_supplicant.conf file directly to pass-in WIFI_SSID and WIFI_PSK values into the config file to be substituted

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -4,5 +4,11 @@ install -v -d						${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d
 install -v -m 644 files/wait.conf			${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/
 
 install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
-install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
+
+workingFile=files/wpa_supplicant.conf.tmp
+cat files/wpa_supplicant.conf | \
+   sed -e "s/{{\ *WIFI_SSID\ *}}/${WIFI_SSID}/g" | \
+   sed -e "s/{{\ *WIFI_PSK\ *}}/${WIFI_PSK}/g" > ${workingFile}
+install -v -m 600 ${workingFile}             ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
+/bin/rm ${workingFile}
 
